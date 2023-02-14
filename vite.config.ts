73077d34c49aba0370b1d4183169e4668ca09509
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-14 10:16:24 +0800

    resetbase

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-pages/dist/',
+  // base: '/github-pages/dist/',
   plugins: [vue()]
 })
