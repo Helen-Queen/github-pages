ecf4a174df34aa62efc668c3014ea4e999b072f7
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-15 13:10:50 +0800

    feature:css初始化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,7 +1,18 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from './Welcom.module.scss'
+import logo from '../assets/icons/山竹.svg'
+// console.log(logo);  ///src/assets/icons/山竹.svg
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    return () => <div><RouterView/></div>;
+    return () => <div class={s.wrapper}>
+    <header>
+        <img src={logo} />
+        <h1>山竹记账</h1>
+    </header>
+    <main><RouterView/></main>
+    
+    </div>
   },
 });
