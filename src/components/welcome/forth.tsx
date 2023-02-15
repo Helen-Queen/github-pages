8b08a5667d5af40b9c348b2542d01d0c2eda31a1
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-15 17:11:16 +0800

    四个页面搞定

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,6 +1,29 @@
 import { defineComponent } from "vue";
+import s from "./First.module.scss";
+import cloud from "../../assets/icons/云.svg";
+import { RouterLink } from "vue-router";
 export const Forth = defineComponent({
   setup: (props, context) => {
-    return () => <div>Forth</div>;
-  },
-});
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={cloud} />
+          <h2>
+            会挣钱
+            <br />
+            还要会省钱
+          </h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/start">完成</RouterLink>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+        </div>
+      </div>
+    )
+  }
+})
