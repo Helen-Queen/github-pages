08aea1e179bd801cbcdc9c3acb425b41616d7917
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-17 10:05:52 +0800

    添加useSwipe Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { defineComponent, ref } from "vue";
-import { RouterView } from "vue-router";
+import { defineComponent, Transition, VNode } from "vue";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import "./App.scss"
 
 export const App = defineComponent({
@@ -8,6 +8,6 @@ export const App = defineComponent({
       <div class="page">
         <RouterView />
       </div>
-    );
-  },
-});
+    )
+  }
+})
--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,23 +1,12 @@
+import s from './welcome.module.scss';
+import { FunctionalComponent } from 'vue';
+export const First: FunctionalComponent = () => {
+  return <div class={s.card}>
+    <svg>
+      <use xlinkHref='#pig'></use>
+    </svg>
+    <h2>会挣钱<br />还会省钱</h2>
+  </div>
+}
 
-import s from "./WelcomeLayout.module.scss";
-import pig from "../../assets/icons/存钱罐.svg";
-import { RouterLink } from "vue-router";
-import { WelcomeLayout } from "./WelcomeLayout";
-export const First = () => {
-    return(
-      <WelcomeLayout>
-        {{
-          icon: () => <img class={s.icon} src={pig}></img>,
-          title:() => <h2>会挣钱<br />还要会省钱</h2>,
-          buttons:() => <>
-            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-            <RouterLink to="/welcome/2">下一页</RouterLink>
-            <RouterLink to="/start">跳过</RouterLink>
-          </>
-        }}
-      </WelcomeLayout>
-    )
-  }
-
-
-First.displayName = "First"
+First.displayName = 'First'
