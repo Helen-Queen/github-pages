43c3d048ef1b09f67a9b2fe69c52f1061344df34
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-22 13:33:51 +0800

    添加navbar组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,6 +2,9 @@ import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
 import { FloatButton } from '../shared/FloatButton';
 import s from './StartPage.module.scss';
+import { Icon } from '../shared/Icon';
+import { Center } from '../shared/Center';
+import { Navbar } from '../shared/Navbar';
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const onClick = () => {
@@ -9,6 +12,15 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
+          <Navbar>{
+            {
+              default: '山竹记账',
+              icon:<Icon name="menu" class={s.navIcon}></Icon>}
+            }
+          </Navbar>
+        <Center class={s.pig_wrapper}>
+          <Icon name="pig" class={s.pig} />
+        </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>测试</Button>
         </div>
